9abeb6a105452d32020d5ec67f3f491ca80f11b1
LuchoQuiru <42559301+LuchoQuiru@users.noreply.github.com>
2021-09-30 19:00:53 -0300

    Test

--- proyecto_APS/src/App.java
+++ proyecto_APS/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //comentario de lucho 82253261354
+        //comentario de lucho 82253261354
     }
 }
